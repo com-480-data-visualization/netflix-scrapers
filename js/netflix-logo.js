fc8a14e705f275fb78e33c4b9b273654205fc6ca
Jeremy Chaverot <100281310+jchaverot@users.noreply.github.com>
2024-04-23 23:35:28 +0200

    add netflix logo animation at website launch

---
+++ js/netflix-logo.js
@@ -0,0 +1,122 @@
+// ---------- NETFLIX ANIMATION ---------- //
+
+/*
+  _   _
+ | \ | |
+ |  \| |
+ | . ` |
+ | |\  |
+ |_| \_| */
+const n_tl = gsap.timeline();
+n_tl
+    .to("#N1-shadow", {opacity: 0, duration: 0.25}, 0.3)
+    .to("#N3-shadow", {opacity: 0, duration: 0.75}, 0.3)
+
+/*
+  ______
+ |  ____|
+ | |__
+ |  __|
+ | |____
+ |______|*/
+const e_ogShape = "M255.1 171.6V208.2L250.5 208.5L250 172L255.1 171.6Z";
+const e_tl = gsap.timeline();
+e_tl
+    .from("#E1-base", {morphSVG:{shape: e_ogShape, type: "linear"}, opacity: 0, duration: 0.075}, 0)
+    .from("#E2-base", {scaleY: 0, transformOrigin: "50% 100%", duration: 0.05}, 0.11)
+    .from("#E3", {scaleX: 0, duration: 0.03}, 0.21)
+    .from("#E4", {scaleX: 0, duration: 0.09}, 0.27)
+    .to("#E1-shadow, #E2-shadow", {opacity: 0, duration: 0.4}, 0)
+
+/*
+  _______
+ |__   __|
+    | |
+    | |
+    | |
+    |_|*/
+const t_tl = gsap.timeline();
+t_tl
+    .from("#T1", {scaleX: 0, duration: 0.05}, 0)
+    .from("#T2-base", {scaleY: 0, duration: 0.125}, 0.1)
+    .to("#T2-shadow", {opacity: 0, duration: 0.41}, 0)
+
+/*
+  ______
+ |  ____|
+ | |__
+ |  __|
+ | |
+ |_|*/
+const f_tl = gsap.timeline();
+f_tl
+    .from("#F1", {scaleX: 0, duration: 0.075}, 0)
+    .from("#F2-base", {scaleY: 0, duration: 0.165}, 0.1)
+    .from("#F3", {scaleX: 0, duration: 0.075}, 0.28)
+    .to("#F2-shadow", {opacity: 0, duration: 0.43}, 0)
+
+/*
+  _
+ | |
+ | |
+ | |
+ | |____
+ |______|*/
+const l_ogShape = "M540.5 167.5L546 167.781V204.371L540.5 204.1V167.5Z";
+const l_tl = gsap.timeline();
+l_tl
+    .from("#L1-base", {scaleY: 0, duration: 0.11}, 0)
+    .from("#L2", {morphSVG:{shape: l_ogShape, type: "linear"}, opacity: 0, duration: 0.05}, 0.2)
+    .to("#L1-shadow", {opacity: 0, duration: 0.415}, 0)
+
+/*
+  _____
+ |_   _|
+   | |
+   | |
+  _| |_
+ |_____|*/
+const i_tl = gsap.timeline();
+i_tl
+    .from("#I", {scaleY: 0, transformOrigin: "50% 100%", duration: 0.09}, 0)
+
+/*
+ __   __
+ \ \ / /
+  \ V /
+   > <
+  / . \
+ /_/ \_\*/
+const x1_ogShape = "M733.1 216.8L771.5 220.8L772.5 218.5L734.5 213.5L733.1 216.8Z";
+const x2_ogShape = "M737 0L738 2.5H777.5L776.5 0H737Z";
+const x_tl = gsap.timeline();
+x_tl
+    .from("#X1", {morphSVG:{shape: x1_ogShape, type: "linear", shapeIndex: 2}, duration: 0.315}, 0)
+    .from("#X1", {opacity: 0, duration: 0.05,}, 0)
+    .from("#X2-base", {morphSVG:{shape: x2_ogShape, type: "linear", shapeIndex: 2}, duration: 0.265}, 0.11)
+    .from("#X2-base", {opacity: 0, duration: 0.005,}, 0.11)
+    .to("#X2-shadow", {opacity: 0, duration: 0.65}, 0)
+
+//Movement Timeline
+const movement_tl = gsap.timeline();
+movement_tl
+    .from("svg", {opacity: 0, duration: 0.35}, 0)
+    .from("svg", {xPercent:50, left:"50%",  duration: 0.95,  ease: CustomEase.create("custom", "M0,0,C0.358,0.144,0.098,1,1,1")}, 0.7)
+
+//Exit Timeline
+const exit_tl = gsap.timeline();
+exit_tl
+    .to("svg", {opacity: 0, duration: 0.25})
+
+//Master Timeline
+const master_tl = gsap.timeline({repeat: -1, repeatDelay: 0.5});
+master_tl
+    .add((movement_tl), 0)
+    .add((n_tl), 0.7)
+    .add((e_tl), 0.8)
+    .add((t_tl), 1.08)
+    .add((f_tl), 1.18)
+    .add((l_tl), 1.33)
+    .add((i_tl), 1.63)
+    .add((x_tl), 1.7)
+    .add((exit_tl), 3)
