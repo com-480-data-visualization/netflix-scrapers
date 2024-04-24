ec3ae4ef62a61cbb7ef257e8d17e903c0ba756cb
jchaverot <jeremy.chaverot@epfl.ch>
2024-04-24 16:38:52 +0200

    center nav bar, increase netflix anim speed

--- js/netflix-logo.js
+++ js/netflix-logo.js
@@ -9,8 +9,8 @@
  |_| \_| */
 const n_tl = gsap.timeline();
 n_tl
-    .to("#N1-shadow", {opacity: 0, duration: 0.25}, 0.3)
-    .to("#N3-shadow", {opacity: 0, duration: 0.75}, 0.3)
+    .to("#N1-shadow", {opacity: 0, duration: 0.167}, 0.3)
+    .to("#N3-shadow", {opacity: 0, duration: 0.5}, 0.3)
 
 /*
   ______
@@ -22,11 +22,11 @@ n_tl
 const e_ogShape = "M255.1 171.6V208.2L250.5 208.5L250 172L255.1 171.6Z";
 const e_tl = gsap.timeline();
 e_tl
-    .from("#E1-base", {morphSVG:{shape: e_ogShape, type: "linear"}, opacity: 0, duration: 0.075}, 0)
-    .from("#E2-base", {scaleY: 0, transformOrigin: "50% 100%", duration: 0.05}, 0.11)
-    .from("#E3", {scaleX: 0, duration: 0.03}, 0.21)
-    .from("#E4", {scaleX: 0, duration: 0.09}, 0.27)
-    .to("#E1-shadow, #E2-shadow", {opacity: 0, duration: 0.4}, 0)
+    .from("#E1-base", {morphSVG:{shape: e_ogShape, type: "linear"}, opacity: 0, duration: 0.05}, 0)
+    .from("#E2-base", {scaleY: 0, transformOrigin: "50% 100%", duration: 0.033}, 0.11)
+    .from("#E3", {scaleX: 0, duration: 0.02}, 0.21)
+    .from("#E4", {scaleX: 0, duration: 0.06}, 0.27)
+    .to("#E1-shadow, #E2-shadow", {opacity: 0, duration: 0.27}, 0)
 
 /*
   _______
@@ -37,8 +37,8 @@ e_tl
     |_|*/
 const t_tl = gsap.timeline();
 t_tl
-    .from("#T1", {scaleX: 0, duration: 0.05}, 0)
-    .from("#T2-base", {scaleY: 0, duration: 0.125}, 0.1)
+    .from("#T1", {scaleX: 0, duration: 0.033}, 0)
+    .from("#T2-base", {scaleY: 0, duration: 0.0833}, 0.1)
     .to("#T2-shadow", {opacity: 0, duration: 0.41}, 0)
 
 /*
@@ -52,8 +52,8 @@ const f_tl = gsap.timeline();
 f_tl
     .from("#F1", {scaleX: 0, duration: 0.075}, 0)
     .from("#F2-base", {scaleY: 0, duration: 0.165}, 0.1)
-    .from("#F3", {scaleX: 0, duration: 0.075}, 0.28)
-    .to("#F2-shadow", {opacity: 0, duration: 0.43}, 0)
+    .from("#F3", {scaleX: 0, duration: 0.05}, 0.28)
+    .to("#F2-shadow", {opacity: 0, duration: 0.2867}, 0)
 
 /*
   _
@@ -65,9 +65,9 @@ f_tl
 const l_ogShape = "M540.5 167.5L546 167.781V204.371L540.5 204.1V167.5Z";
 const l_tl = gsap.timeline();
 l_tl
-    .from("#L1-base", {scaleY: 0, duration: 0.11}, 0)
-    .from("#L2", {morphSVG:{shape: l_ogShape, type: "linear"}, opacity: 0, duration: 0.05}, 0.2)
-    .to("#L1-shadow", {opacity: 0, duration: 0.415}, 0)
+    .from("#L1-base", {scaleY: 0, duration: 0.0733}, 0)
+    .from("#L2", {morphSVG:{shape: l_ogShape, type: "linear"}, opacity: 0, duration: 0.033}, 0.2)
+    .to("#L1-shadow", {opacity: 0, duration: 0.2767}, 0)
 
 /*
   _____
@@ -78,7 +78,7 @@ l_tl
  |_____|*/
 const i_tl = gsap.timeline();
 i_tl
-    .from("#I", {scaleY: 0, transformOrigin: "50% 100%", duration: 0.09}, 0)
+    .from("#I", {scaleY: 0, transformOrigin: "50% 100%", duration: 0.06}, 0)
 
 /*
  __   __
@@ -91,22 +91,22 @@ const x1_ogShape = "M733.1 216.8L771.5 220.8L772.5 218.5L734.5 213.5L733.1 216.8
 const x2_ogShape = "M737 0L738 2.5H777.5L776.5 0H737Z";
 const x_tl = gsap.timeline();
 x_tl
-    .from("#X1", {morphSVG:{shape: x1_ogShape, type: "linear", shapeIndex: 2}, duration: 0.315}, 0)
-    .from("#X1", {opacity: 0, duration: 0.05,}, 0)
-    .from("#X2-base", {morphSVG:{shape: x2_ogShape, type: "linear", shapeIndex: 2}, duration: 0.265}, 0.11)
-    .from("#X2-base", {opacity: 0, duration: 0.005,}, 0.11)
-    .to("#X2-shadow", {opacity: 0, duration: 0.65}, 0)
+    .from("#X1", {morphSVG:{shape: x1_ogShape, type: "linear", shapeIndex: 2}, duration: 0.21}, 0)
+    .from("#X1", {opacity: 0, duration: 0.033,}, 0)
+    .from("#X2-base", {morphSVG:{shape: x2_ogShape, type: "linear", shapeIndex: 2}, duration: 0.1767}, 0.11)
+    .from("#X2-base", {opacity: 0, duration: 0.0033,}, 0.11)
+    .to("#X2-shadow", {opacity: 0, duration: 0.4334}, 0)
 
 //Movement Timeline
 const movement_tl = gsap.timeline();
 movement_tl
-    .from("svg", {opacity: 0, duration: 0.35}, 0)
-    .from("svg", {xPercent:50, left:"50%",  duration: 0.95,  ease: CustomEase.create("custom", "M0,0,C0.358,0.144,0.098,1,1,1")}, 0.7)
+    .from("svg", {opacity: 0, duration: 0.2334}, 0)
+    .from("svg", {xPercent:50, left:"50%",  duration: 0.6333,  ease: CustomEase.create("custom", "M0,0,C0.358,0.144,0.098,1,1,1")}, 0.7)
 
 //Exit Timeline
 const exit_tl = gsap.timeline();
 exit_tl
-    .to("svg", {opacity: 0, duration: 0.25})
+    .to("svg", {opacity: 0, duration: 0.1667})
 
 //Master Timeline
 const master_tl = gsap.timeline({repeat: -1, repeatDelay: 0.5});
