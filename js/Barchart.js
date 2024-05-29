4cfd807cfc21e49308bc0016234a55132e90d199
jchaverot <jeremy.chaverot@epfl.ch>
2024-05-30 00:35:11 +0200

    Barchart version with tooltip not working

--- js/Barchart.js
+++ js/Barchart.js
@@ -1,16 +1,14 @@
-
-
 function showBubbleChart(year, imdbRange) {
     // Your D3 code to draw the bubble chart
 
 
-        document.getElementById('back-button').style.display = 'block';
+    document.getElementById('back-button').style.display = 'block';
 
-        $.ajax({
+    $.ajax({
         url: 'data/others/detailed_data.json',
         dataType: 'json',
         async: false,
-        success: function(data) {
+        success: function (data) {
             const filteredData = data.filter(item => item.rating_range === imdbRange && item.release_year === year);
 
             // Aggregate actors by genre for total count and top actors
@@ -46,10 +44,18 @@ function showBubbleChart(year, imdbRange) {
             }));
             const diameter = 400;
             const svg = d3.select("#chart-container").html('') // Clear the previous chart
-                    .append("svg")
-                    .attr("width", diameter)
-                    .attr("height", diameter);
+                .append("svg")
+                .attr("width", diameter)
+                .attr("height", diameter);
 
+            var tooltip = svg.append('div')
+                .style('opacity', 0)
+                .attr('class', 'tooltip')
+                .style('background-color', 'black')
+                .style('border-radius', '15px')
+                .style('padding', '12px')
+                .style('color', 'white')
+                .style('width', '275px');
 
             const bubble = d3.pack()
                 .size([diameter, diameter])
@@ -62,7 +68,6 @@ function showBubbleChart(year, imdbRange) {
                 .padding(2);
 
             const nodes = bubbleLayout(root).leaves();  // Generating nodes
-
 
 
             const node = svg.selectAll(".node")
@@ -81,34 +86,39 @@ function showBubbleChart(year, imdbRange) {
                 .text(d => d.data.genre.substring(0, d.r / 3))
                 .attr("font-size", d => d.r / 5);
 
-                node.on("click", d => {
-        const data = d.target.__data__;
-        const infoDiv = document.getElementById("info");
-        if (data.data.topActors) {
-            // Create HTML content for the list of top actors
-            let content = `<h3>Top Actors in ${data.data.genre}</h3><ul>`;
-            data.data.topActors.forEach(actor => {
-                content += `<li>${actor.name} (Score: ${actor.score})</li>`;
-            });
-            content += `<p>Total Actors: ${data.data.total}</p>`;
-            content += '</ul>';
+            node.on("mouseover", d => {
+                const data = d.target.__data__;
+                if (data.data.topActors) {
+                    let content = `<h3>Top Actors in ${data.data.genre}</h3><ul>`;
+                    data.data.topActors.forEach(actor => {
+                        content += `<li>${actor.name} (Score: ${actor.score})</li>`;
+                    });
+                    content += `<p>Total Actors: ${data.data.total}</p>`;
+                    content += '</ul>';
 
-            // Display the content in the info div
-            infoDiv.innerHTML = content;
-            infoDiv.style.display = 'block'; // Make the info div visible
-        } else {
-            infoDiv.innerHTML = '<p>No actor data available for this genre.</p>';
-            infoDiv.style.display = 'block';
+                    tooltip.html(content)
+                        .style("left", (d.pageX + 10) + "px")
+                        .style("top", (d.pageY - 10) + "px")
+                        .style('opacity', 1);
+                } else {
+                    // If no data available, display a message
+                    tooltip.html('<p>No actor data available for this genre.</p>')
+                        .style("left", (d.pageX + 10) + "px")
+                        .style("top", (d.pageY - 10) + "px")
+                        .style('opacity', 1);
+                }
+            })
+                .on("mouseout", () => {
+                    tooltip.style('opacity', 0);
+                });
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error('Error loading JSON data:', textStatus, errorThrown);
+        },
+        complete: function () {
+            console.log('Loaded movies and shows successfully.');
         }
     });
-  },
-  error: function(jqXHR, textStatus, errorThrown) {
-         console.error('Error loading JSON data:', textStatus, errorThrown);
-     },
-     complete: function() {
-         console.log('Loaded movies and shows successfully.');
-     }
-        });
 
 }
 
@@ -122,51 +132,50 @@ function loadDaviz(dimensions) {
         url: 'data/others/detailed_data.json',
         dataType: 'json',
         async: false,
-        success: function(data) {
+        success: function (data) {
             //console.log(data);
             let release_years = [...new Set(data.map(item => item.release_year))].sort();
             //release_years=release_years.filter(item=> item.release_year>=2000);
             const ranges = [...new Set(data.map(item => item.rating_range))];
             //console.log(ranges);
             const traces = ranges.map(range => {
-    let countsPerYear = new Array(release_years.length).fill(0);
-    let namesPerYear = new Array(release_years.length).fill('');
-    let genresPerYear=new Array(release_years.length).fill('');
-    let custom=new Array(release_years.length).fill()
+                    let countsPerYear = new Array(release_years.length).fill(0);
+                    let namesPerYear = new Array(release_years.length).fill('');
+                    let genresPerYear = new Array(release_years.length).fill('');
+                    let custom = new Array(release_years.length).fill()
 
-    // Go through each year and calculate the total counts and names for that year and range.
-    release_years.forEach((year, index) => {
-    // Filter the data for the current year and rating range.
-    let itemsForYearAndRange = data.filter(item => item.rating_range === range && item.release_year === year);
+                    // Go through each year and calculate the total counts and names for that year and range.
+                    release_years.forEach((year, index) => {
+                        // Filter the data for the current year and rating range.
+                        let itemsForYearAndRange = data.filter(item => item.rating_range === range && item.release_year === year);
 
-    // Assuming item.name is an array of names, sum the lengths for the count.
-    countsPerYear[index] = itemsForYearAndRange
-        .map(item => item.name.length)
-        .reduce((a, b) => a + b, 0);
+                        // Assuming item.name is an array of names, sum the lengths for the count.
+                        countsPerYear[index] = itemsForYearAndRange
+                            .map(item => item.name.length)
+                            .reduce((a, b) => a + b, 0);
 
-    // Join the names for custom data, assuming each item.name is an array of actor names.
-    namesPerYear[index] = itemsForYearAndRange
-        .flatMap(item => item.name) // Flatten the array of names arrays into a single array.
-        .join(', '); // Join all names with a comma.
-    genresPerYear[index] = itemsForYearAndRange.flatMap(item => item.genres).join(', ');
-    custom[index]= { genre: genresPerYear[index], names: namesPerYear[index]}
+                        // Join the names for custom data, assuming each item.name is an array of actor names.
+                        namesPerYear[index] = itemsForYearAndRange
+                            .flatMap(item => item.name) // Flatten the array of names arrays into a single array.
+                            .join(', '); // Join all names with a comma.
+                        genresPerYear[index] = itemsForYearAndRange.flatMap(item => item.genres).join(', ');
+                        custom[index] = {genre: genresPerYear[index], names: namesPerYear[index]}
 
-    });
+                    });
 
 
-                return {
-                    x: release_years,
-                    y: countsPerYear,
-                    name: range,
-                    type: 'bar',
-                    customdata: custom, // Include name as custom data
-                    hoverinfo: 'y+name'
-                };
+                    return {
+                        x: release_years,
+                        y: countsPerYear,
+                        name: range,
+                        type: 'bar',
+                        customdata: custom, // Include name as custom data
+                        hoverinfo: 'y+name'
+                    };
 
-            }
-          );
-          //console.log(traces);
-
+                }
+            );
+            //console.log(traces);
 
 
             const layout = {
@@ -182,25 +191,25 @@ function loadDaviz(dimensions) {
 
             Plotly.newPlot('chart-container', traces, layout);
 
-            document.getElementById('chart-container').on('plotly_click', function(data){
-              const imdbRange = data.points[0].data.name;
-              const year = data.points[0].x;
-              showBubbleChart(year, imdbRange);
-            //window.location.href = `index.html?range=${encodeURIComponent(imdbRange)}&year=${encodeURIComponent(year)}`;
-              });
-            },
-        error: function(jqXHR, textStatus, errorThrown) {
+            document.getElementById('chart-container').on('plotly_click', function (data) {
+                const imdbRange = data.points[0].data.name;
+                const year = data.points[0].x;
+                showBubbleChart(year, imdbRange);
+                //window.location.href = `index.html?range=${encodeURIComponent(imdbRange)}&year=${encodeURIComponent(year)}`;
+            });
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
             console.error('Error loading JSON data:', textStatus, errorThrown);
         },
-        complete: function() {
+        complete: function () {
             console.log('Loaded movies and shows successfully.');
         }
-        });
-  // This will switch to the bubble chart
+    });
+    // This will switch to the bubble chart
 }
 
 
-function showDaviz(dimensions){
+function showDaviz(dimensions) {
 
     width = dimensions.width;
     height = dimensions.height;
